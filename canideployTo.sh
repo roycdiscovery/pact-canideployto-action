d2ad85c810ee24f3b6dd72b8442e457d896a0820
Roy Collings <roy_collings@discovery.com>
2021-04-29 11:39:20 +0200

    Trying custom action

--- canideployTo.sh
+++ canideployTo.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 echo """
-broker_url: $broker_url
-broker_token : $broker_token
+PACT_BROKER: $PACT_BROKER
+PACT_BROKER_TOKEN : $PACT_BROKER_TOKEN
 participant_name: $participant_name
 target_tag: $target_tag
 """
 
 docker run --rm \
- -e PACT_BROKER_BASE_URL=$broker_url \
- -e PACT_BROKER_TOKEN=$broker_token \
+ -e PACT_BROKER_BASE_URL=$PACT_BROKER \
+ -e PACT_BROKER_TOKEN=$PACT_BROKER_TOKEN \
 pactfoundation/pact-cli:latest \
 broker can-i-deploy \
 --pacticipant "$participant_name" \
