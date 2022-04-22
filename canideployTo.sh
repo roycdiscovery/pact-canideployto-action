b4be2f0fee223009605646a74331c24897c37987
Roy Collings <roy_collings@discovery.com>
2022-04-22 16:34:12 +0200

    Added unit tests

--- canideployTo.sh
+++ canideployTo.sh
@@ -10,16 +10,31 @@ VERSION="--latest"
 [ "$version" != "" ] && VERSION="--version $version"
 [ "$latest" != "" ] && VERSION="--latest $latest"
 
-echo "ACTION ENV = pact_broker: $pact_broker, pact_broker_token: $pact_broker_token, application_name: $application_name, version flag: '$VERSION', to: $to"
+MISSING=()
+[ ! "$pact_broker" ] && MISSING+=( "pact_broker" )
+[ ! "$pact_broker_token" ] && MISSING+=( "pact_broker_token" )
+[ ! "$application_name" ] && MISSING+=( "application_name" )
+[ ! "$to" ] && MISSING+=( "to" )
 
+if [ ${#MISSING[@]} -gt 0 ]
+then
+  echo "ERROR: The following environment variables are not set:"
+  printf '\t%s\n' "${MISSING[@]}"
+  exit 1
+fi
 
-eval """
+echo "
+  pact_broker: '$pact_broker'
+  pact_broker_token: '$pact_broker_token'
+  application_name: '$application_name'
+  VERSION: '$VERSION'
+  to: $to"
+
 docker run --rm \
- -e PACT_BROKER_BASE_URL=$pact_broker \
- -e PACT_BROKER_TOKEN=$pact_broker_token \
-pactfoundation/pact-cli:latest \
-broker can-i-deploy \
---pacticipant "$application_name" \
-$VERSION \
---to $to
-"""
+  -e PACT_BROKER_BASE_URL=$pact_broker \
+  -e PACT_BROKER_TOKEN=$pact_broker_token \
+  pactfoundation/pact-cli:latest \
+  broker can-i-deploy \
+  --pacticipant "$application_name" \
+  $VERSION \
+  --to $to
